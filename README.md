ad9c622f72116388e98d411f9dfc00865966865e
mose <mose@gandi.net>
2016-05-06 14:21:12 +0800

    initial push

---
+++ README.md
@@ -0,0 +1,4 @@
+Trellograph
+===========
+
+Just a small script to generate json datapoints for a graph of trello activity.
---
+++ stats.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "encoding/json"
+  "github.com/VojtechVitek/go-trello"
+)
+
+type Config struct {
+  ApiKey      string
+  Token       string
+  BoardsId    []string
+}
+
+func main() {
+
+  file, _ := os.Open("config.json")
+  decoder := json.NewDecoder(file)
+  config := Config{}
+  err := decoder.Decode(&config)
+  if err != nil {
+    fmt.Println("error:", err)
+    return
+  }
+
+  trello, err := trello.NewAuthClient(config.ApiKey, &config.Token)
+  if err != nil {
+    fmt.Println("error:", err)
+    return
+  }
+
+  for boardid := range config.BoardsId {
+    board, err := trello.Board(config.BoardsId[boardid])
+    if err != nil {
+      fmt.Println("error:", err)
+      return
+    }
+    fmt.Printf("%s\n", board.Name)
+    
+  }
+
+}
