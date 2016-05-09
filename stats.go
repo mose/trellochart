44ce20c96a1276bc8a0628b9614501ce42a27abc
mose <mose@gandi.net>
2016-05-09 13:02:00 +0800

    building some templates

--- stats.go
+++ stats.go
@@ -4,6 +4,9 @@ import (
   "os"
   "fmt"
   "time"
+  "regexp"
+  "strings"
+  "text/template"
   "encoding/json"
   "github.com/VojtechVitek/go-trello"
 )
@@ -15,12 +18,13 @@ type Config struct {
 }
 
 type ListsStats struct {
-  Listname      string   `json:"name"`
+  Name          string   `json:"name"`
   Count         int      `json:"count"`
 }
 
 type BoardStats struct {
-  Boardname    string       `json:"name"`
+  Name          string       `json:"name"`
+  Urlname       string       `json:"urlname"`
   Lists        []ListsStats `json:"lists"`
 }
 
@@ -29,55 +33,70 @@ type Stats struct {
   Boards    []BoardStats  `json:"boards"`
 }
 
-func main() {
+func buildIndex(s Stats) {
+  const index = `
+<html><head><title>Trello Stats</title></head><body><ul>
+{{range .Boards}}<li><a href="{{.Urlname}}.html">{{.Name}}</a></li>
+{{end}}
+</ul></body></html>
+`
+  t := template.New("Index")
+  t, _ = t.Parse(index)
+  t.Execute(os.Stdout, s)
+}
 
+func check(e error) {
+  if e != nil {
+    fmt.Println("error: ", e)
+    panic(e)
+  }
+}
+
+func urlize(s string) string {
+  reg, _ := regexp.Compile("[^A-Za-z0-9]+")
+  s = reg.ReplaceAllString(s, "-")
+  s = strings.ToLower(strings.Trim(s, "-"))
+  return s
+}
+
+func main() {
   file, _ := os.Open("config.json")
   decoder := json.NewDecoder(file)
   config := Config{}
   stats := Stats{}
   stats.DateTaken = time.Now().Format("Sat Mar  7 11:06:39 PST 2015")
   err := decoder.Decode(&config)
-  if err != nil {
-    fmt.Println("error:", err)
-    return
-  }
+  check(err)
 
   trello, err := trello.NewAuthClient(config.ApiKey, &config.Token)
-  if err != nil {
-    fmt.Println("error:", err)
-    return
-  }
+  check(err)
 
   user, err := trello.Member(config.User)
-  if err != nil {
-    fmt.Println("error:", err)
-    return
-  }
+  check(err)
 
   boards, err := user.Boards()
-  if err != nil {
-    fmt.Println("error:", err)
-    return
-  }
+  check(err)
 
   if len(boards) > 0 {
     for _, board := range boards {
       boardStat := BoardStats{}
-      boardStat.Boardname = board.Name
-      fmt.Printf("Board: %s\n", board.Name)
+      boardStat.Name = board.Name
+      boardStat.Urlname = urlize(board.Name)
+      // fmt.Printf("Board: %s\n", board.Name)
       lists, err := board.Lists()
-      if err != nil {
-        fmt.Println("error:", err)
-        return
-      }
+      check(err)
       for _, list := range lists {
-        fmt.Printf("|  list: %s\n", list.Name)
+        // fmt.Printf("|  list: %s\n", list.Name)
         cards, _ := list.Cards()
         boardStat.Lists = append(boardStat.Lists, ListsStats{list.Name, len(cards)} )
       }
       stats.Boards = append(stats.Boards, boardStat)
     }
   }
+  // fmt.Println("-------------------\n")
+  // fmt.Printf("%+v\n", stats)
   fmt.Println("-------------------\n")
-  fmt.Printf("%+v\n", stats)
+  buildIndex(stats)
+
+
 }
